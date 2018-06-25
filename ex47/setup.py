0d7459814af27d16a6802d90c8344a81d46f2120
aung-kyaw-khaing <akklove8@gmail.com>
2018-06-25 23:09:59 +0630

    done

---
+++ ex47/setup.py
@@ -0,0 +1,18 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+'description': 'My Project',
+'author': 'Arthur Min Sein',
+'url': 'URL to get it at.',
+'download_url': 'Where to download it.',
+'author_email': 'arthurmin93@gmail.com',
+'version': '0.1',
+'install_requires': ['nose'],
+'packages': ['ex47'],
+'scripts': [],
+'name': 'projectname'
+}
+setup(**config)
