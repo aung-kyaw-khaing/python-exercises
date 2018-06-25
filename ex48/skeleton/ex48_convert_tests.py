34a418e46db1e921f2e2653083c9b40c192adb1d
aung-kyaw-khaing <akklove8@gmail.com>
2018-06-25 23:28:57 +0630

    done

---
+++ ex48/skeleton/ex48_convert_tests.py
@@ -0,0 +1,4 @@
+import ex48_convert as ob
+
+ob.convert_number(20)
+ob.convert_number("something")
---
+++ ex48/skeleton/lexicon.py
@@ -0,0 +1,5 @@
+class lexicon(object):
+    def scan(self, direction):
+        self. direction=direction
+        tup=('direction', self.direction)
+        return list(tup);
